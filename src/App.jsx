04b6d9f05da0d45047d79a4ed054a3424455c85d
Alex-2699 <alexantonio262699@gmail.com>
2022-05-13 00:09:05 -0500

    Se desarrollaron todas las funciones de la calculadora incluyendo el apartado que muestra el historial del almacenamiento local

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,84 @@
+import React, { useState } from "react";
+
 function App() {
+  //Use State que maneja datos de input y variable resultado
+  const [inputState, setinputState] = useState({
+    operacion: "",
+    resultado: "",
+  });
+
+  //Se obtiene todos los datos del almacenamiento local referente al item historial
+  const inicioEstado = JSON.parse(localStorage.getItem("historial")) || [];
+  const [historial, setHistorial] = useState(inicioEstado); // Se inicializa el state con la variable anterior
+
+  const handleInputChange = (event) => {
+    setinputState({
+      ...inputState,
+      [event.target.name]: event.target.value,
+    });
+  };
+
+  //Agrega al state el número presionado del botón
+  const handleNumeroClick = (event) => {
+    setinputState({
+      ...inputState,
+      operacion: inputState.operacion + event.target.value,
+    });
+  };
+
+  //Agrega el valor del operador presionado
+  const handleOperadorClick = (event) => {
+    setinputState({
+      ...inputState,
+      operacion: inputState.operacion + event.target.textContent,
+    });
+  };
+
+  //Procesa la expresión y obtiene el resultado
+  const handleResultado = () => {
+    var datos = inputState.operacion; //Se almacena la expresión del input en una variable
+    var res = eval(datos); //Ejecuta la expresión almacenada en la variable
+
+    //Agrega al almacenmiento local el JSON de la operación realizada
+    localStorage.setItem(
+      "historial",
+      JSON.stringify([
+        ...historial,
+        {
+          operacion: datos,
+          resultado: res,
+        },
+      ])
+    );
+    //Actualiza el state que obtiene el historial de operaciones
+    setHistorial([
+      ...historial,
+      {
+        operacion: datos,
+        resultado: res,
+      },
+    ]);
+
+    //Actualiza el state del input para mostrar el resultado en pantalla
+    setinputState({
+      ...inputState,
+      operacion: res,
+      resultado: "",
+    });
+  };
+
+  //Reinicia los estados del input
+  const handleClickLimpiar = () => {
+    setinputState({
+      ...inputState,
+      operacion: "",
+      resultado: "",
+    });
+  };
+
   return (
     <div className="row" style={{ width: 950 }}>
-      <div className="col" style={{ background: "#D8F5FF" }}>
+      <div className="col">
         <div className="row card m-4 p-3 shadow" style={{ width: 400 }}>
           <div className="ms-2 me-2 mt-2 row">
             <div className="col">
@@ -14,6 +91,10 @@ function App() {
                       <input
                         style={{ fontSize: "2rem", textAlign: "right" }}
                         id="operacion"
+                        name="operacion"
+                        type="text"
+                        onChange={handleInputChange}
+                        value={inputState.operacion}
                       ></input>
                     </span>
                     <br></br>
@@ -23,7 +104,11 @@ function App() {
                 <div className="row">
                   <div className="col">
                     <span className="row ms-0 me-0">
-                      <button type="button" className="btn btn-warning">
+                      <button
+                        type="button"
+                        className="btn btn-warning"
+                        onClick={handleClickLimpiar}
+                      >
                         Limpiar
                       </button>
                     </span>
@@ -41,7 +126,11 @@ function App() {
 
                       <div className="col">
                         <span className="row ms-0 me-0">
-                          <button type="button" className="btn btn-warning">
+                          <button
+                            type="button"
+                            className="btn btn-warning"
+                            onClick={handleOperadorClick}
+                          >
                             /
                           </button>
                         </span>
@@ -53,7 +142,12 @@ function App() {
                 <div className="row">
                   <div className="col">
                     <span className="row mt-3 ms-0 me-0">
-                      <button type="button" className="btn btn-primary">
+                      <button
+                        type="button"
+                        className="btn btn-primary"
+                        value={7}
+                        onClick={handleNumeroClick}
+                      >
                         7
                       </button>
                     </span>
@@ -61,7 +155,12 @@ function App() {
 
                   <div className="col">
                     <span className="row mt-3 ms-0 me-0">
-                      <button type="button" className="btn btn-primary">
+                      <button
+                        type="button"
+                        className="btn btn-primary"
+                        value={8}
+                        onClick={handleNumeroClick}
+                      >
                         8
                       </button>
                     </span>
@@ -69,7 +168,12 @@ function App() {
 
                   <div className="col">
                     <span className="row mt-3 ms-0 me-0">
-                      <button type="button" className="btn btn-primary">
+                      <button
+                        type="button"
+                        className="btn btn-primary"
+                        value={9}
+                        onClick={handleNumeroClick}
+                      >
                         9
                       </button>
                     </span>
@@ -77,8 +181,12 @@ function App() {
 
                   <div className="col">
                     <span className="row mt-3 ms-0 me-0">
-                      <button type="button" className="btn btn-warning">
-                        X
+                      <button
+                        type="button"
+                        className="btn btn-warning"
+                        onClick={handleOperadorClick}
+                      >
+                        *
                       </button>
                     </span>
                   </div>
@@ -87,7 +195,12 @@ function App() {
                 <div className="row">
                   <div className="col">
                     <span className="row mt-3 ms-0 me-0">
-                      <button type="button" className="btn btn-primary">
+                      <button
+                        type="button"
+                        className="btn btn-primary"
+                        value={4}
+                        onClick={handleNumeroClick}
+                      >
                         4
                       </button>
                     </span>
@@ -95,7 +208,12 @@ function App() {
 
                   <div className="col">
                     <span className="row mt-3 ms-0 me-0">
-                      <button type="button" className="btn btn-primary">
+                      <button
+                        type="button"
+                        className="btn btn-primary"
+                        value={5}
+                        onClick={handleNumeroClick}
+                      >
                         5
                       </button>
                     </span>
@@ -103,7 +221,12 @@ function App() {
 
                   <div className="col">
                     <span className="row mt-3 ms-0 me-0">
-                      <button type="button" className="btn btn-primary">
+                      <button
+                        type="button"
+                        className="btn btn-primary"
+                        value={6}
+                        onClick={handleNumeroClick}
+                      >
                         6
                       </button>
                     </span>
@@ -111,7 +234,11 @@ function App() {
 
                   <div className="col">
                     <span className="row mt-3 ms-0 me-0">
-                      <button type="button" className="btn btn-warning">
+                      <button
+                        type="button"
+                        className="btn btn-warning"
+                        onClick={handleOperadorClick}
+                      >
                         -
                       </button>
                     </span>
@@ -121,7 +248,12 @@ function App() {
                 <div className="row">
                   <div className="col">
                     <span className="row mt-3 ms-0 me-0">
-                      <button type="button" className="btn btn-primary">
+                      <button
+                        type="button"
+                        className="btn btn-primary"
+                        value={1}
+                        onClick={handleNumeroClick}
+                      >
                         1
                       </button>
                     </span>
@@ -129,7 +261,12 @@ function App() {
 
                   <div className="col">
                     <span className="row mt-3 ms-0 me-0">
-                      <button type="button" className="btn btn-primary">
+                      <button
+                        type="button"
+                        className="btn btn-primary"
+                        value={2}
+                        onClick={handleNumeroClick}
+                      >
                         2
                       </button>
                     </span>
@@ -137,7 +274,12 @@ function App() {
 
                   <div className="col">
                     <span className="row mt-3 ms-0 me-0">
-                      <button type="button" className="btn btn-primary">
+                      <button
+                        type="button"
+                        className="btn btn-primary"
+                        value={3}
+                        onClick={handleNumeroClick}
+                      >
                         3
                       </button>
                     </span>
@@ -145,7 +287,11 @@ function App() {
 
                   <div className="col">
                     <span className="row mt-3 ms-0 me-0">
-                      <button type="button" className="btn btn-warning">
+                      <button
+                        type="button"
+                        className="btn btn-warning"
+                        onClick={handleOperadorClick}
+                      >
                         +
                       </button>
                     </span>
@@ -157,7 +303,12 @@ function App() {
                     <div className="row">
                       <div className="col">
                         <span className="row ms-0 me-0">
-                          <button type="button" className="btn btn-primary">
+                          <button
+                            type="button"
+                            className="btn btn-primary"
+                            value={0}
+                            onClick={handleNumeroClick}
+                          >
                             0
                           </button>
                         </span>
@@ -165,7 +316,12 @@ function App() {
 
                       <div className="col">
                         <span className="row ms-0 me-0">
-                          <button type="button" className="btn btn-primary">
+                          <button
+                            type="button"
+                            className="btn btn-primary"
+                            value={"."}
+                            onClick={handleNumeroClick}
+                          >
                             .
                           </button>
                         </span>
@@ -175,7 +331,11 @@ function App() {
 
                   <div className="col">
                     <span className="row ms-0 me-0">
-                      <button type="button" className="btn btn-warning">
+                      <button
+                        type="button"
+                        className="btn btn-warning"
+                        onClick={handleResultado}
+                      >
                         =
                       </button>
                     </span>
@@ -191,16 +351,27 @@ function App() {
 
       {/*Aqí empieza el div del historial*/}
 
-      <div
-        className="col"
-        style={{ width: 400, background: "#5C5C5C" }}
-      >
+      <div className="col" style={{ width: 400 }}>
         <span className="row">
           <div
             className="row card m-4 p-3 shadow"
-            style={{ width: 400, left: 0, height:440 }}
+            style={{ width: 400, left: 0 }}
           >
             <h3>Historial de operaciones</h3>
+
+            {historial.length === 0 ? (
+              "Vacío"
+            ) : (
+              <ul>
+                {historial.map((item, index) => {
+                  return (
+                    <li className="fs-5">
+                      {item.operacion} = {item.resultado} &nbsp;
+                    </li>
+                  );
+                })}
+              </ul>
+            )}
           </div>
         </span>
       </div>
